c85ae7cbcc6ccbff8617463061df97861dd7d695
Mahmud Kandawala <mahmud.kandawala1@gmail.com>
2022-11-04 19:54:40 -0400

    coding

--- Flask Web/website/views.py
+++ Flask Web/website/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint #bunch of urls
 
 views = Blueprint('views', __name__)
 
-@views.root('/') #url (root)
+@views.route('/') #url (route)
 def home():
     return "<h1>Test</h1>"
 

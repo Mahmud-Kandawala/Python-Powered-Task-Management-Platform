1375068bad60215216785e4fbdcdce5bf45d2d84
Mahmud Kandawala <mahmud.kandawala1@gmail.com>
2022-11-05 17:03:54 -0400

    coding

--- Flask Web/website/auth.py
+++ Flask Web/website/auth.py
@@ -1,15 +1,20 @@
-from flask import Blueprint , render_template
+from flask import Blueprint , render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    return render_template("login.html", )
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
     return render_template("sign_up.html")
